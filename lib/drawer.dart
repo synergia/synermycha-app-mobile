0cd2268eb8192f6470990baa35893043502285f4
Stanisław <stanislaw.dac@gmail.com>
2021-02-18 18:08:09 +0100

    zmieniono styl tekstu

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -3,56 +3,60 @@ import 'package:flutter/material.dart';
 import "config.dart";
 import "bluetoothConfig.dart";
 
-
 class WidgetDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('SynerMycha'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'SynerMycha',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
               ),
             ),
-            ListTile(
-              title: Text('eBike Configuration'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          EBikeConfigurationMenu(title: "eBike Configuration")),
-                );
-              },
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
             ),
-            ListTile(
-              title: Text('App Configuration'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AppConfigurationMenu()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Bluetooth config'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FindDevicesScreen()),
-                );
-              },
-            ),
-          ],
-        ),
+          ),
+          ListTile(
+            title: Text('eBike Configuration'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        EBikeConfigurationMenu(title: "eBike Configuration")),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('App Configuration'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AppConfigurationMenu()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Bluetooth config'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FindDevicesScreen()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
