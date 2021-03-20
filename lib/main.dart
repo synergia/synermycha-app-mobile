981d524b30675097edf289497f67de2f068ec395
Stanisław <stanislaw.dac@pwr.edu.pl>
2021-03-20 16:48:19 +0100

    display SimpleDialog when tapping on title

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SynerMycha',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: PageChooseDevice(),
--- lib/pages/page_device.dart
+++ lib/pages/page_device.dart
@@ -8,47 +8,13 @@ class PageDevice extends StatelessWidget {
   // final BluetoothDevice synermycha;
 
   PageDevice({Key key, @required this.bluetoothManager}) : super(key: key);
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: Colors.transparent,
-  //       elevation: 0,
-  //       iconTheme: IconThemeData(color: Colors.black),
-  //       leading: IconButton(
-  //         tooltip: MaterialLocalizations.of(context).closeButtonTooltip,
-  //         icon: const Icon(Icons.close),
-  //         onPressed: () {},
-  //       ),
-  //       title: Text(
-  //         "Łącze się...",
-  //         style: TextStyle(color: Colors.black),
-  //       ),
-  //     ),
-  //     body: Center(
-  //         child: FutureBuilder<SynerMycha>(
-  //       future: bluetoothManager.setupSynermycha(),
-  //       builder: (context, AsyncSnapshot<SynerMycha> snapshot) {
-  //         if (snapshot.hasData) {
-  //           return Text(snapshot.data.device.name);
-  //         } else if (snapshot.hasError) {
-  //           return Text("${snapshot.error}");
-  //         }
-  //         // By default, show a loading spinner
-  //         return CircularProgressIndicator();
-  //       },
-  //     )),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder<SynerMycha>(
         future: bluetoothManager.setupSynermycha(),
         builder: (context, AsyncSnapshot<SynerMycha> snapshot) {
           if (snapshot.hasData) {
-            return _buildView(context, snapshot.data.device);
+            return _buildView(context, snapshot.data);
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
           }
@@ -57,25 +23,35 @@ class PageDevice extends StatelessWidget {
         });
   }
 
-  Widget _buildView(context, device) {
+  Widget _buildView(context, synermycha) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-        leading: IconButton(
-          tooltip: MaterialLocalizations.of(context).closeButtonTooltip,
-          icon: const Icon(Icons.close),
-          onPressed: () {
-            device.disconnect();
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          device.name,
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          leading: IconButton(
+            tooltip: MaterialLocalizations.of(context).closeButtonTooltip,
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              synermycha.device.disconnect();
+              Navigator.pop(context);
+            },
+          ),
+          title: InkWell(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (ctxt) => new AlertDialog(
+                    title: Text("Device information"),
+                    // content: Text(synermycha.deviceInfo.softwareRevision),
+                  )
+              );
+            },
+            child: Text(
+              synermycha.device.name,
+              style: TextStyle(color: Colors.black),
+            ),
+          )),
       body: Center(),
     );
   }
